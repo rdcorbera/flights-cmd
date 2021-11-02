14c0b27e85f2f4b006b95908d8c02b699a959f35
Ricardo Corbera <rdcorbera@gmail.com>
2021-11-02 20:49:15 -0300

    Initial project

---
+++ src/main/java/com/skyxairlines/services/flightcmd/FlightCmdApplication.java
@@ -0,0 +1,13 @@
+package com.skyxairlines.services.flightcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightCmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightCmdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skyxairlines/services/flightcmd/FlightCmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyxairlines.services.flightcmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightCmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
